a79449cd24fc6a49ae4e5d7c1fe3d32543789533
Matias <59626616+mativironi@users.noreply.github.com>
2020-05-25 19:24:42 -0300

    Create cd.mk

---
+++ Comandos/cd.mk
@@ -0,0 +1,21 @@
+# CD
+* **Nombre:** _cd - change directory_
+* **Sintaxis:** cd _[directorio]_
+* **Descripcion:** _Cambiar el directorio de trabajo_
+* **Ejemplos**
+
+```cd``` Me envia a mi directorio personal. Ej (home\pi).
+
+```cd ..``` Regresa al directorio anterior (Directorio Padre).
+
+```cd .``` Se mantiene en el directorio actual.
+
+```cd /``` Cambia al directorio raiz (Ruta Absoluta).
+
+```cd pi/Documents``` Cambio al directorio Documents (Ruta Relativa).
+
+> La ruta del directorio puede ser Absoluta o Relativa.
+
+> El simbolo ~ (virgulilla) me indica que estoy en mi directorio personal.
+
+> El directorio home contiene subdirectorios por cada uno de los usuarios del sistema.
